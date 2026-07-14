09a20ab2680a34088dd296ae1fb6e054e905d00e
SyedSultan0 <syedsultan.dev@gmail.com>
2026-07-14 16:02:38 +0530

    Implement input handling for fuel fraction

--- Week 3 - Exceptions/fuel.py
+++ Week 3 - Exceptions/fuel.py
@@ -1,1 +1,29 @@
+while True:
+    try:
+        a = input("Fraction: ")
+        x, y = a.split("/")
 
+        if "." in x or "." in y:
+            raise ValueError
+
+        x = int(x)
+        y = int(y)
+
+        if y == 0 or x < 0 or y < 0 or x > y:
+            raise ValueError
+
+        value = x / y
+
+        if value <= 0.01:
+            print("E")
+        elif value >= 0.99:
+            print("F")
+        else:
+            print(f"{round(value * 100)}%")
+
+        break
+
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
