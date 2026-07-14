46a77b496b89849f57ad5d09a6ce409be0375da1
SyedSultan0 <syedsultan.dev@gmail.com>
2026-07-14 15:45:23 +0530

    Add deep.py to check answer to the Great Question

---
+++ Week 0 - Functions, Variables/Week 1 - Conditionals/deep.py
@@ -0,0 +1,5 @@
+enter = input("What is the answer to the Great Question of Life, the Universe and Everything: ").strip().lower()
+if enter == 42 or enter == "42" or enter == "forty-two" or enter == "forty two":
+    print("Yes")
+else:
+    print("No")
